886280df0db07331e954c0bd676e8fc93c6b82ef
Rajesh7889 <khajuriarajesh768@gmail.com>
2023-05-08 20:56:32 +0530

    form phase3

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 function validate(){
+    let error1=document.getElementById('error1');
+    let error2=document.getElementById('error2');
     let name=document.getElementById('name');
     let password=document.getElementById('password');
-    if(name.value.trim()==''||password.value.trim()==''){
-        alert('no blank values allowed');
+
+    if(name.value.trim()==''){
+        error1.style.visibility='visible';
+        error1.innerHTML='*please enter name';
+        name.style.border='solid 2px red';
         return false;
     }else {
-      true;
+        error1.innerHTML= '<br>';
+        error1.style.visibility='disable';
+        name.style.border='solid 2px green';
+    }
+    if(password.value.trim()=='') {
+        error2.style.visibility='visible';
+        error2.innerHTML='*please enter password';
+        password.style.border='solid 2px red';
+        return false;
+    }else if(password.value.length<5){
+        error2.innerHTML='*length of password must be greater than 5';
+        password.style.border='solid 2px red';
+        return false;
+    }else{
+        error2.innerHTML='<br>';
+        error2.style.visibility='disable';
+        password.style.border='solid 2px green';
+        return true;
     }
 }
